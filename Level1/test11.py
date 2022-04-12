b440fb490125a5926f3f035259190e2278fbb5ba
GoWoong <popgun10@naver.com>
2022-04-12 23:51:12 +0900

    2022-04-12 add 2 problem

--- Level1/test11.py
+++ Level1/test11.py
@@ -1,1 +1,15 @@
-s = []
+def solution(d, budget):
+    answer = 0
+    d.sort()
+    for i in range(len(d)):
+        if d[i] <= budget:
+            answer += 1
+            budget -= d[i]
+        else:
+            break
+    return answer
+
+d = [1,3,2,5,4]
+budget = 9
+
+print(solution(d,budget))
---
+++ Level1/test12.py
@@ -0,0 +1,21 @@
+def solution(left, right):
+    result = []
+    answer = 0
+    for i in range(left, right+1):
+        result.append(0)
+        for j in range(1,i+1):
+            if(i % j == 0):
+                result[len(result)-1] += 1
+        if result[len(result)-1] % 2 == 0:
+            answer += i
+        else:
+            answer -= i
+    return answer
+
+
+left1 = 13
+left2 = 24
+right1 = 17
+right2 = 27
+print(solution(left1,right1))
+print(solution(left2,right2))
