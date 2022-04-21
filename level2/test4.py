5946460342a016180256e06a1054c0f13da92108
GoWoong <popgun10@naver.com>
2022-04-21 15:46:49 +0900

    2022-04-21 15:46

--- level2/test4.py
+++ level2/test4.py
@@ -1,3 +1,9 @@
-def solution(progresses, speeds):
-    answer = []
-    return answer
+def solution(citations):
+    citations.sort()
+    print(citations)
+    for i in range(0,len(citations)):
+        if citations[i] >= len(citations)-i:
+            return len(citations)-i
+    return 0
+citations = [3, 0, 6, 1, 5]
+print(solution(citations))
